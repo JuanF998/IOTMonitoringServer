89758256fd8fac9be6b6da94226be25640b29d97
Juan Felipe Tamayo <j.tamayoo@uniandes.edu.co>
2023-09-21 21:28:32 -0500

    mensaje commit

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.82.250.11"]
+ALLOWED_HOSTS = ["localhost", "44.201.10.221"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.175.70.53",  # Dirección IP de la base de datos
+        "HOST": "44.193.206.249",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.88.164.8"
+MQTT_HOST = "3.239.200.216"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
